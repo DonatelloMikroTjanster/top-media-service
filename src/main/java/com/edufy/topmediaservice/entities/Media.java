283fae7367f5cef6b670a46d7b775f3c69f55f55
AbaSheger <111607144+AbaSheger@users.noreply.github.com>
2024-09-30 14:51:01 +0200

    added several fields

--- src/main/java/com/edufy/topmediaservice/entities/Media.java
+++ src/main/java/com/edufy/topmediaservice/entities/Media.java
@@ -3,6 +3,7 @@ package com.edufy.topmediaservice.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,11 +18,20 @@ public class Media {
     @Column(name = "title", length = 100)
     private String title;
 
+    @Column(name = "media_type", nullable = false, length = 100)
+    private String mediaCategory;
+
     @Column(name = "genre", nullable = false, length = 100)
     private String genre;
 
-    @Column(name = "media_category", length = 100)
-    private String mediaCategory;
+    @Column(name = "release_date", nullable = false, length = 100)
+    private LocalDate releaseDate;
+
+    @Column(name = "url", nullable = false, length = 100)
+    private String url;
+
+    @Column(name = "duration", nullable = false, length = 100)
+    private String duration;
 
     @OneToMany(mappedBy = "media", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JsonIgnore
@@ -29,14 +39,6 @@ public class Media {
 
     public Media() {
 
-    }
-
-    public Set<TopMedia> getTopMedia() {
-        return topMedia;
-    }
-
-    public void setTopMedia(Set<TopMedia> topMedia) {
-        this.topMedia = topMedia;
     }
 
     public Long getId() {
@@ -69,5 +71,37 @@ public class Media {
 
     public void setGenre(String genre) {
         this.genre = genre;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public Set<TopMedia> getTopMedia() {
+        return topMedia;
+    }
+
+    public void setTopMedia(Set<TopMedia> topMedia) {
+        this.topMedia = topMedia;
     }
 }
